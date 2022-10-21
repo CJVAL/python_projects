c9a49f8fac87cf60e9f1d974b2ae67564ba5a21f
CJVAL <99738723+CJVAL@users.noreply.github.com>
2022-10-21 17:51:59 +0100

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,6 @@
+from tkinter.messagebox import YES
+
+
 print("Welcome to my computer quiz!")
 
 playing = input("Would you like to play? ")
@@ -11,25 +14,31 @@ print(f"Your score is currently {score}")
 answer = input("What does CPU stand for? ")
 if answer.lower() == 'central processing unit':
     print("Yes! That's correct")
-    score = score + 1
-    print(f"Your score is currently {score}")
+    score += 1
+    #score(f"Your score is currently {score}")
 else:
     print("I'm sorry that's incorrect")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == 'graphics processing unit':
     print("Yes! That's correct")
+    score += 1
 else:
     print("I'm sorry that's incorrect")
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == 'random access memory':
     print("Yes! That's correct")
+    score += 1
 else:
     print("I'm sorry that's incorrect")
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == 'power supply unit':
     print("Yes! That's correct")
+    score += 1
 else:
     print("I'm sorry that's incorrect")
+
+print(f"You have reached the end of the quiz. \nYour score was {score}")
+print(f"You got {score/4*100}% correct")
